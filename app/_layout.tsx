6cf0607b1e7095d8af53a5dbc16f28a2001dacc4
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2024-12-22 23:13:38 +0100

    feat: bottom tab icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,66 @@
-import { Stack } from "expo-router";
+import { Image } from "react-native";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{ title: "Home" ,
+
+          tabBarIcon: () => (
+            <Image
+              source={require("../assets/home-2.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+        // component={require("./index").default}
+      />
+      <Tabs.Screen name="discover" options={{ title: "Discover",
+          tabBarIcon: () => (
+            <Image
+              source={require("../assets/discover.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+       }} />
+
+      <Tabs.Screen
+        name="dirvethru"
+        options={{
+          title: "Drive Thru",
+          tabBarIcon: () => (
+            <Image
+              source={require("../assets/car.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="orders"
+        options={{
+          title: "Orders",
+          tabBarIcon: () => (
+            <Image
+              source={require("../assets/receipt.webp")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: () => (
+            <FontAwesome name="user-circle" size={24} color="black" />
+          ),
+        }}
+        // component={require("./profile").default}
+      />
+    </Tabs>
   );
 }
