39c22c7d5136f998e141337fff1fb613e6028ff4
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2024-12-24 10:13:02 +0100

    feat: auth screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ export default function Layout() {
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="onboarding"  options={{ headerShown: false }} />
+      <Stack.Screen name="auth"  options={{ headerShown: false }} />
     </Stack>
   );
 }
